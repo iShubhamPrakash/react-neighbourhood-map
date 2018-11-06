f2fe2dbfcce50c7e12000f2cfe571f6aaca7d2a5
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-11-07 05:25:46 +0530

    Add sidebar container

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Header from "./Header"
+import SideBar from "./SideBar"
 
 class App extends Component {
   render() {
     return (
       <div className="main-app">
-        <Header/>
+        <Header />
+        <SideBar/>
+
       <h1>Hello World!</h1>
     </div>
     );
---
+++ src/components/SideBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// This component is for sidebar on left of the app
+function SideBar() {
+
+    return (
+        <div className="side-bar">
+            <h1>test side bar</h1>
+        </div>
+        );
+    }
+
+export default SideBar;
