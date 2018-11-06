8be549d95806a4e1c40267d0b800515703a0514b
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-11-07 04:55:02 +0530

    Add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import '../App.css';
+import Header from "./Header"
 
 class App extends Component {
   render() {
     return (
+      <div className="main-app">
+        <Header/>
       <h1>Hello World!</h1>
+    </div>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// This component is for header at the top of the app
+function Header() {
+
+    return (
+        <div className="header-container">
+            <div className="menu-button">
+                <img src={require('./Hamburger_icon.png')} alt="" id="menu-button"/>
+            </div>
+            <div className="header-text">
+                <h1>Neighbourhood Map</h1>
+            </div>
+        </div>
+        );
+    }
+
+export default Header;
