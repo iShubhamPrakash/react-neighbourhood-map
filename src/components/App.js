aaa517d4c41c62cd392938947d8e66c5c8fca628
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-11-07 05:49:25 +0530

    Add map using google map API

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,62 @@ import Header from "./Header"
 import SideBar from "./SideBar"
 
 class App extends Component {
-  render() {
+
+  state = {
+
+  }
+
+  componentDidMount() {
+    window.initMap = this.initMap;
+    // Asynchronously load the Google Maps script, passing in the callback reference
+    loadMapScript(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBI20h8IjJ3gP82J6MTxEJMlwY4K7nsFak&callback=initMap"
+    );
+  }
+
+  initMap = () => {
+    // Default values when the map loads for the first time
+    let defaultLatitude = 25.607794;
+    let defaultLongitude = 85.137722;
+    let defaultZoom = 16;
+    let mapContainer = document.getElementById("map");
+    // set full height for the map
+    mapContainer.style.height = window.innerHeight - 66 + "px";
+
+    let self = this;
+
+    let map = new window.google.maps.Map(mapContainer, {
+      center: { lat: defaultLatitude, lng: defaultLongitude },
+      zoom: defaultZoom,
+      mapTypeControl: false
+    });
+
+  }
+
+  render(){
     return (
       <div className="main-app">
         <Header />
         <SideBar/>
 
-      <h1>Hello World!</h1>
+      <div id="map" />
     </div>
     );
   }
 }
 
 export default App;
+
+
+// To add script to load google map
+
+function loadMapScript(src) {
+  let ref = window.document.getElementsByTagName("script")[0];
+  let script = window.document.createElement("script");
+  script.async = true;
+  script.src = src;
+  script.onerror = function() {
+    document.write("Error: Google Maps can't be loaded");
+  };
+  ref.parentNode.insertBefore(script, ref);
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+// Render App component inside the div with id root
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
